df8570992c5ab31000c967a50c835ef0a8dfa6e3
tigermei <tigermei@tencent.com>
2021-03-09 15:24:47 +0800

    test code

---
+++ example/lib-tigermei/src/main/java/com/example/lib_tigermei/MyTestLibrary.java
@@ -0,0 +1,10 @@
+package com.example.lib_tigermei;
+
+public class MyTestLibrary {
+    public MyTestLibrary(){}
+
+    public static void TestMain(){
+        System.out.println("Hello, MyTestLibrary!!");
+        return;
+    }
+}
